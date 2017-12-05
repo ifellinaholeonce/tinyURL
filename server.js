9159689ae0ee29c974004b34a62a22438c0ea726
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-05 22:14:18 +0000

    redirects to shortened urls

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
-var urlDB = {
+let urlDB = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -20,12 +20,14 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = urlDB;
   res.render("urls_index", {templateVars});
+  console.log(templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL);
-  generateRandomString(req.body.longURL);
-  res.send("Ok");
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString(longURL);
+  urlDB[shortURL] = longURL;
+  res.redirect(303, `/urls/${shortURL}`);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -35,27 +37,41 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = {shortURL: req.params.id, urlDB };
-  res.render("urls_show", templateVars);
+  if (urlDB[req.params.id]) {
+    res.render("urls_show", templateVars);
+  } else {
+    res.redirect(404, "/urls/new");
+  }
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDB);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  if (urlDB[shortURL]) {
+    res.redirect(307, urlDB[shortURL]);
+  } else {
+    res.redirect(404, "/urls/new");
+    console.log(urlDB[shortURL]);
+  }
+});
+
 ////////////////////////////////////////////////////
 //////////////RANDOM NUMBER GEN/////////////////////
 ////////////////////////////////////////////////////
-function generateRandomString() {
-    length = 6;
-    chars = '#aA';
-    var mask = '';
-    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
-    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (chars.indexOf('#') > -1) mask += '0123456789';
-    var result = '';
-    for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
-    return result;
-}
+let generateRandomString = () => {
+  let length = 6;
+  let chars = '#aA';
+  var mask = '';
+  if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+  if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  if (chars.indexOf('#') > -1) mask += '0123456789';
+  var result = '';
+  for (var i = length; i > 0; --i) result += mask[Math.round(Math.random() * (mask.length - 1))];
+  return result;
+};
 
 ////////////////////////////////////////////////////
 //////////////////START APP/////////////////////////
