452ccf69b2c9a3833a1fefe1ede8d40a3603a407
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-06 19:13:07 +0000

    URLs belong to users

--- models/urls.js
+++ models/urls.js
@@ -1,6 +1,12 @@
 const urlDB = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "user_id": "userRandomID"
+  },
+  "9sm5xK": {
+    "9sm5xK": "http://www.google.com",
+    "user_id": "userRandomID"
+  }
 };
 
 module.exports = {
