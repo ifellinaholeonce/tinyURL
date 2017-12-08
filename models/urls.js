638c836d7dacfc9fab36face724428487d9a8b6e
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-08 13:18:24 +0000

    Adjusted urls objected to start handling timestamps

--- models/urls.js
+++ models/urls.js
@@ -3,13 +3,25 @@ const urlDB = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "user_id": "userRandomID",
     "visits": 10,
-    "uniqueVisits": 1
+    "uniqueVisits": 1,
+    "timestampCreated": {
+      "YYYY": 2017,
+      "MM": 07,
+      "DD": 18,
+      "time": "18:23"
+    }
   },
   "9sm5xK": {
     "9sm5xK": "http://www.google.com",
     "user_id": "userRandomID",
     "visits": 4,
-    "uniqueVisits": 4
+    "uniqueVisits": 4,
+    "timestampCreated": {
+      "YYYY": 2017,
+      "MM": 07,
+      "DD": 18,
+      "time": "18:23"
+    }
   }
 };
 
