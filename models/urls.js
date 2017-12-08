781e3381d2e6778b5abe8cedd201061467ec03e0
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-08 13:09:24 +0000

    touched up how analytics was presented on urls_show and added analytics to urls_index."

--- models/urls.js
+++ models/urls.js
@@ -2,12 +2,14 @@ const urlDB = {
   "b2xVn2": {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "user_id": "userRandomID",
-    "visits": 10
+    "visits": 10,
+    "uniqueVisits": 1
   },
   "9sm5xK": {
     "9sm5xK": "http://www.google.com",
     "user_id": "userRandomID",
-    "visits": 4
+    "visits": 4,
+    "uniqueVisits": 4
   }
 };
 
